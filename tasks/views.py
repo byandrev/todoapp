e1deca0db40f931a8036d163819c811474fc1426
MauricioDDS <mauriciodidonato794@gmail.com>
2024-04-18 10:29:31 -0500

    chore: create task model

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 
+from .models import Task
 
 def index(request):
-    return HttpResponse("Hello")
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return HttpResponse(tasks)
