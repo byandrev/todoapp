392209d01cb6ac730647887737262a1bff55a0ef
anderssonccg <anderssonccg@gmail.com>
2024-04-21 11:39:56 -0500

    chore: add new fields to task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,8 +1,15 @@
-from django.db import models
-
-# Create your models here.
 
 from django.db import models
+from django.db.models.functions import Now
+
+class Status(models.TextChoices):
+    COMPLETED = "Completed"
+    PROGRESS = "In Progress"
+    PENDING = "Pending"
+
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
+    status = models.CharField(default=Status.PENDING, max_length=100)
+    limit_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(db_default=Now())
