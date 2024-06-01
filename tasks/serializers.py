1409144d04e897a534fbae0f1eefba9dd503dde6
Andres Parra <byandrev@gmail.com>
2024-05-31 19:50:45 -0500

    refactor: update TaskSerializer Include priority field and set user_id during creation

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,21 +1,12 @@
 from rest_framework import serializers
 from .models import Task
-from users.models import User
 
 
-class TaskSerialize(serializers.HyperlinkedModelSerializer):
-    user_id = serializers.SlugRelatedField(
-        queryset=User.objects.all(), slug_field="username"
-    )
-
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
-        fields = (
-            "id",
-            "name",
-            "status",
-            "description",
-            "created_at",
-            "limit_date",
-            "user_id",
-        )
+        fields = ("id", "name", "status", "priority", "description", "created_at", "limit_date")
+
+    def create(self, validated_data):
+        validated_data["user_id"] = self.context["request"].user
+        return super(TaskSerializer, self).create(validated_data)
