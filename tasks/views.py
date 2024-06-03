fb8d3c7c82096da0419f4f8ba038e83ffc3d3c97
MauricioDDS <mauriciodidonato794@gmail.com>
2024-06-03 18:47:53 -0500

    fix: Restore deleted lines of code and add new backend filters

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .models import Task
 from .serializers import TaskSerializer
-from rest_framework import viewsets
-from django_filters.rest_framework import DjangoFilterBackend
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['status']
+    filter_backends = [OrderingFilter, DjangoFilterBackend, SearchFilter]
+    search_fileds = ["name", "description"]
+    fielteset_fields = ['status']
+
+    def get_queryset(self):
+        return self.request.user.tasks.all()
