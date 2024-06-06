7bf3bb9eccbc5432df2cb6ecde488c10fcecfdd0
Andres Parra <byandrev@gmail.com>
2024-06-05 20:48:37 -0500

    feat: add user creation view to urls

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from users.views import UserCreationView
 
 router = routers.DefaultRouter()
 router.register(r"tasks", TaskViewSet)
@@ -15,5 +16,6 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/users", UserCreationView.as_view()),
     path("api/", include(router.urls)),
 ]
