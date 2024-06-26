92750ee909cc75e7f2dfedf809632e4036fda75c
Andres Parra <byandrev@gmail.com>
2024-06-26 12:22:20 -0500

    feat: add verify token route

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,6 +6,7 @@ from users.views import UserCreationView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 from users.views import UserCreationView
 
@@ -17,7 +18,7 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/users", UserCreationView.as_view()),
+    path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
     path("api/", include(router.urls)),
     path("api/users/", UserCreationView.as_view(), name="user_create"),
 ]
